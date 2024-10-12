490bd31c9d2332df098deb270f66584eb1dfd39e
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-10-12 19:35:09 +0530

    add: middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+// via nextjs official documentary
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/login', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/view-appointments',
+}
